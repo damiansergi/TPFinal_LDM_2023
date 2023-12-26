82b0a7c32ec83b6d6995f84b1f6a876850073759
Gonzalo Linares <golinares@itba.edu.ar>
2023-12-26 00:23:42 -0300

    added more lines

--- LED_Matrix/source/LEDMatrix.c
+++ LED_Matrix/source/LEDMatrix.c
@@ -1,6 +1,15 @@
 
 #include "LEDMatrix.h"
+#include "PWM.h"
+
+#define TICKPERPERIOD (130) // 100MHz * 1.3us = 130, porque es el mas critico
+
+#define RESET (5000)
 
 void initLEDMatrix()
 {
+    // System clk = 100MHz
+    PWM_Init(); // Initializamos el PWM
+    PWM_SetTickPerPeriod(TICKPERPERIOD);
+    PWM_SetDC();
 }

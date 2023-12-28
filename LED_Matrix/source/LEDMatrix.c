c73b9b19acc17a759333120757bf65c82dda2e98
Gonzalo Linares <golinares@itba.edu.ar>
2023-12-28 03:42:21 -0300

    cambio menor

--- LED_Matrix/source/LEDMatrix.c
+++ LED_Matrix/source/LEDMatrix.c
@@ -7,7 +7,6 @@
 #include "config.h"
 
 #define TICKSPERPERIOD (63) // 50MHz * 1.25us
-#define RESET (1200)         // 60us
 #define T0H (20)
 #define T1H (40)
 #define NUMOFLEDS (64)
@@ -16,9 +15,9 @@
 
 typedef enum
 {
-    RED,
-    BLUE,
-    GREEN,
+	RED,
+	BLUE,
+	GREEN,
 } states;
 
 static color_t LEDMatrix[NUMOFLEDS] = {0};
@@ -31,93 +30,93 @@ static void onRefreshEnded();
 
 void initLEDMatrix()
 {
-    initPIT();
-    PWM_Init();
-    PWM_SetTickPerPeriod(TICKSPERPERIOD);
-    RGB2PWM();
-    FTM_SetInterruptionCallback(onRefreshEnded);
-    PWM_GenWaveform(PWMLEDMatrix, NUMOFLEDS * RGBBITS, sizeof(uint16_t));
+	initPIT();
+	PWM_Init();
+	PWM_SetTickPerPeriod(TICKSPERPERIOD);
+	RGB2PWM();
+	FTM_SetInterruptionCallback(onRefreshEnded);
+	PWM_GenWaveform(PWMLEDMatrix, NUMOFLEDS * RGBBITS, sizeof(uint16_t));
 
-    refreshTimerID = createTimer(REFRESHRATE, refresh);
-    startTimer(refreshTimerID);
+	refreshTimerID = createTimer(REFRESHRATE, refresh);
+	startTimer(refreshTimerID);
 }
 
 static void refresh()
 {
-    FTM_StartClock(FTM0);
+	FTM_StartClock(FTM0);
 }
 
 static void onRefreshEnded()
 {
-    FTM_StopClock(FTM0);
+	FTM_StopClock(FTM0);
 }
 
 void changeColor(uint8_t row, uint8_t col, color_t color)
 {
-    LEDMatrix[row * 8 + col] = color;
-    RGB2PWM();
+	LEDMatrix[row * 8 + col] = color;
+	RGB2PWM();
 }
 
 static void RGB2PWM()
 {
-    int currBit = 7;
-    states currColor = GREEN;
-    uint8_t bit = 0;
+	int currBit = 7;
+	states currColor = GREEN;
+	uint8_t bit = 0;
 
-    for (int i = 0; i < NUMOFLEDS; i++)
-    {
-        for (int j = 0; j < RGBBITS; j++)
-        {
-            switch (currColor)
-            {
-				case GREEN:
-					bit = (LEDMatrix[i].g >> currBit) & (0x1);
-					if (currBit <= 0)
-					{
-						currColor = RED;
-						currBit = 7;
-					}
-					else
-					{
-						currBit--;
-					}
-					break;
+	for (int i = 0; i < NUMOFLEDS; i++)
+	{
+		for (int j = 0; j < RGBBITS; j++)
+		{
+			switch (currColor)
+			{
+			case GREEN:
+				bit = (LEDMatrix[i].g >> currBit) & (0x1);
+				if (currBit <= 0)
+				{
+					currColor = RED;
+					currBit = 7;
+				}
+				else
+				{
+					currBit--;
+				}
+				break;
 
-				case RED:
-					bit = (LEDMatrix[i].r >> currBit) & (0x1);
-					if (currBit <= 0)
-					{
-						currColor = BLUE;
-						currBit = 7;
-					}
-					else
-					{
-						currBit--;
-					}
-					break;
+			case RED:
+				bit = (LEDMatrix[i].r >> currBit) & (0x1);
+				if (currBit <= 0)
+				{
+					currColor = BLUE;
+					currBit = 7;
+				}
+				else
+				{
+					currBit--;
+				}
+				break;
 
-				case BLUE:
-					bit = (LEDMatrix[i].g >> currBit) & (0x1);
-					if (currBit <= 0)
-					{
-						currColor = GREEN;
-						currBit = 7;
-					}
-					else
-					{
-						currBit--;
-					}
-					break;
+			case BLUE:
+				bit = (LEDMatrix[i].g >> currBit) & (0x1);
+				if (currBit <= 0)
+				{
+					currColor = GREEN;
+					currBit = 7;
+				}
+				else
+				{
+					currBit--;
+				}
+				break;
 			}
 
-            if (bit == 0)
-            {
-                PWMLEDMatrix[i * RGBBITS + j] = T0H;
-            }
-            else
-            {
-                PWMLEDMatrix[i * RGBBITS + j] = T1H;
-            }
-        }
-    }
+			if (bit == 0)
+			{
+				PWMLEDMatrix[i * RGBBITS + j] = T0H;
+			}
+			else
+			{
+				PWMLEDMatrix[i * RGBBITS + j] = T1H;
+			}
+		}
+	}
 }
